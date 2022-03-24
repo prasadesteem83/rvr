d1e865d34d6b0d0dee057e634a7199556442274f
Rajendra <rajendrapj@gmail.com>
2022-03-24 19:52:52 -0400

    2commit

--- src/AplletApp/Master/MasterApp.component.ts
+++ src/AplletApp/Master/MasterApp.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './MasterApp.componentView.html',
-  styleUrls: ['./MasterApp.component.css']
+  styleUrls: ['./MasterApp.component.css'] 
 })
 export class MasterAppComponent {
   title = 'test1';
